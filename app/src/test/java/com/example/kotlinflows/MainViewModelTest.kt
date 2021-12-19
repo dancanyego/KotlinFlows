ddcf402f13dbb5d52a81a3c0cc540f88d5ed59a4
dancanyego <dancankipchumba76@gmail.com>
2021-12-19 03:43:46 +0300

    Test

--- app/src/test/java/com/example/kotlinflows/MainViewModelTest.kt
+++ app/src/test/java/com/example/kotlinflows/MainViewModelTest.kt
@@ -1,12 +1,26 @@
 package com.example.kotlinflows
 
-import org.junit.Assert.*
-
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 
 class MainViewModelTest {
 
+    private lateinit var viewModel: MainViewModel
+
     @Before
     fun setUp() {
+        viewModel = MainViewModel()
+    }
+    @Test
+    fun `countDownFlow , Properly Counts Down from 5 to 0`() = runBlocking{
+        viewModel.countDownFlow.test {
+            for (i in 5 downTo 0){
+                val emission = awaitItem()
+                assertThat(emission).isEqualTo(i)
+            }
+        }
     }
 }
