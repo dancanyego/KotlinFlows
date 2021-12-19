2500f62cd549b5ffa91e2e99bba2e5ab4e0b6566
dancanyego <dancankipchumba76@gmail.com>
2021-12-19 04:19:23 +0300

    Test Dispatcher

--- app/src/test/java/com/example/kotlinflows/MainViewModelTest.kt
+++ app/src/test/java/com/example/kotlinflows/MainViewModelTest.kt
@@ -2,25 +2,31 @@ package com.example.kotlinflows
 
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class MainViewModelTest {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var testDispatchers: TestDispatchers
 
     @Before
     fun setUp() {
-        viewModel = MainViewModel()
+        testDispatchers = TestDispatchers()
+        viewModel = MainViewModel(testDispatchers)
     }
     @Test
     fun `countDownFlow , Properly Counts Down from 5 to 0`() = runBlocking{
         viewModel.countDownFlow.test {
             for (i in 5 downTo 0){
+                testDispatchers.testDispatcher.advanceTimeBy(1000L)
                 val emission = awaitItem()
                 assertThat(emission).isEqualTo(i)
             }
+            cancelAndConsumeRemainingEvents()
         }
     }
 }
